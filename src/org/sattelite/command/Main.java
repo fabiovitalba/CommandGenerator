e5c1daa802c58fa9055e4bc5245b2823455925d0
Fabio.Vitalba <fabvit9@gmail.com>
2024-05-23 18:36:01 +0200

    Add next problem we're going to solve

--- src/org/sattelite/command/Main.java
+++ src/org/sattelite/command/Main.java
@@ -4,7 +4,11 @@ public class Main {
 	
 	public static void main(String[] args){
 		TurnCommand command = new TurnCommand("SLOW", 180, 60, 45); 
-		
+
+		// What if we wanted to create something like this:
+		// Compressed + Properties
+		// ... but we want to avoid duplicate code!
+
 		CompressedXMLSerializer cxs = new CompressedXMLSerializer();
 		cxs.generateFile("command.zip", command);
 		
