541b3e0692a32e4fa0877c79a41871c0dddb2f5d
Fabio.Vitalba <fabvit9@gmail.com>
2024-05-23 18:48:51 +0200

    Add solution to initial request

--- src/org/sattelite/command/Main.java
+++ src/org/sattelite/command/Main.java
@@ -8,16 +8,21 @@ public class Main {
 	public static void main(String[] args){
 		TurnCommand command = new TurnCommand("SLOW", 180, 60, 45); 
 
-		// What if we wanted to create something like this:
-		// Compressed + Properties
-		// ... but we want to avoid duplicate code!
-
+		/*
 		XMLSerializer cxs = new XMLSerializer(new Compressor());
 		cxs.generateFile("command.zip", command);
 		
-		PropertiesSerializer sps = new PropertiesSerializer(new Encriptor(5));
+		PropertiesSerializer sps = new PropertiesSerializer(new Encriptor((byte)5));
 		sps.generateFile("command.txt", command);
-		
+		*/
+
+
+		// What if we wanted to create something like this:
+		// Compressed + Properties
+		// ... but we want to avoid duplicate code!
+		PropertiesSerializer sps = new PropertiesSerializer(new Compressor());
+		sps.generateFile("new_command.zip", command);
+
 	}
 
 }
